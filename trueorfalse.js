14cf005882e6d30ab7646ab5c1c9dfa2869846f7
arifahan <95140381+arifahan@users.noreply.github.com>
2022-01-19 08:47:55 +0600

    ture

---
+++ trueorfalse.js
@@ -0,0 +1,15 @@
+// Falsy
+// 0
+// null
+// NuN
+// udenfined
+// ""
+// false
+let age = 15;
+if(age || age ==0){
+    console.log ("Condition is true");
+
+}
+else{
+    console.log ("Condition is false");
+}
