d238e65736129fd872476e3cf229ce1fe3c47337
arifahan <95140381+arifahan@users.noreply.github.com>
2022-01-19 09:03:40 +0600

    NuLL

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+undefined 
+     let pakhi;
+
+
+ function add (num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+const result = add(13, 13);
+console.log(result);
+
+
+ function add (num1, num2){
+    console.log(num1, num2);
+
+}
+
+
+const premika = {name:"smart dude", phone:124564};
+console.log(premika.gm);
+
+
+let fun = undefined;
