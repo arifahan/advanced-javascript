855844f6ec91834ee6bacdee9f1fe91c1c58212f
arifahan <95140381+arifahan@users.noreply.github.com>
2022-01-21 14:53:27 +0600

    ok

--- map-filter.js
+++ map-filter.js
@@ -1,1 +1,24 @@
-console.log(24.4);
+const numbers = [4, 5 , 6, 7, 8, 9];
+// const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element
+//     output.push(result);
+// }
+
+// const square = element=>element*element;
+// const square = x => x * x;
+
+
+
+
+// const result = numbers.map(function(element){
+//     return element * element;
+// })
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+const result = numbers.filter(x => x * x);
+
+console.log(result);
